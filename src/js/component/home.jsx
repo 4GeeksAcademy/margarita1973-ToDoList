465c4bb1c9f933c018a763fb7fb8135435bc6ab1
Rosario Alonso <79224753+margarita1973@users.noreply.github.com>
2024-10-12 22:35:37 +0000

    margarita1973 - Todo List primera version

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,23 +2,18 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import NewItem from "./newitem";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="display-1">todos</h1>
+			<div className="todolist-body d-flex m-auto">
+				<NewItem />
+			</div>
+			<div className="back d-flex m-auto z-n1"></div>
+			<div className="back d-flex m-auto z-n2"></div>
 		</div>
 	);
 };
---
+++ src/js/component/newitem.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const NewItem = () => {
+    const [newToDo, setNewToDo] = useState("")
+    const [toDos, setToDos] = useState([])
+
+    const deleteItem = (i) => {const actualizeList = toDos.filter((todo, position) => position !== i)
+        setToDos(actualizeList);}
+
+    function itemleft () {
+        let items = 0;
+        items = toDos.length;
+        return items
+     }
+
+   
+   
+    return (
+        <div id="todolist" className="container border border-secondary-subtle p-0">
+            <input id="input" className="form-control  px-5 py-3" placeholder="what's need to be done?" onChange={
+                (e) => { setNewToDo(e.target.value) }}
+                onKeyDown={(e) => { if (e.key == "Enter") { setToDos([...toDos, newToDo]); e.target.value = "" } }}
+            />
+            <ul className="list-group"> 
+                {toDos.map((todo, i) => {
+                return(
+                <li className="list-group-item text-start px-5 py-3"> {todo} 
+                <button type="button" className="btn-close position-absolute end-0 me-2" onClick={()=>deleteItem(i)}></button>
+                </li>)
+            })}
+            </ul>
+<p className="counter"> {itemleft()} items left</p>
+        </div>
+    )
+}
+export default NewItem;
+
+
